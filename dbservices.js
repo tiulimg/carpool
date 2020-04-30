fe641d473d1104d4fbe654fa0f3922bb0272824e
tiulimg <tiulimg@gmail.com>
2020-04-30 12:56:26 +0300

    resolve

--- dbservices.js
+++ dbservices.js
@@ -25,22 +25,25 @@ var IRONNUMBERS_COLLECTION = "ironnumbers";
 var ROUTES_COLLECTION = "routes";
 
 function initialize(app) {
-    // Connect to the database before starting the application server.
-    var mongoClient = new mongodb.MongoClient(process.env.MONGODB_URI || "mongodb://localhost:27017/test",{ useUnifiedTopology: true });
-    mongoClient.connect(function (err, client) {
-        if (err) {
-            console.log(err);
-            process.exit(1);
-        }
+    return new Promise((resolve, reject) => {
+        // Connect to the database before starting the application server.
+        var mongoClient = new mongodb.MongoClient(process.env.MONGODB_URI || "mongodb://localhost:27017/test",{ useUnifiedTopology: true });
+        mongoClient.connect(function (err, client) {
+            if (err) {
+                console.log(err);
+                process.exit(1);
+            }
 
-        // Save database object from the callback for reuse.
-        db = client.db();
-        console.log("Database connection ready");
+            // Save database object from the callback for reuse.
+            db = client.db();
+            console.log("Database connection ready");
 
-        // Initialize the app.
-        var server = app.listen(process.env.PORT || 8080, function () {
-            var port = server.address().port;
-            console.log("App now running on port", port);
+            // Initialize the app.
+            var server = app.listen(process.env.PORT || 8080, function () {
+                var port = server.address().port;
+                console.log("App now running on port", port);
+                return resolve();
+            });
         });
     });
 }
