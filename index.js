6251af2dcb509f01deef3b67104233423b45794b
tiulimg <tiulimg@gmail.com>
2020-04-30 12:59:58 +0300

    can't listen same port twice

--- index.js
+++ index.js
@@ -48,26 +48,26 @@ dbservices.initialize(app)
 });
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
+// var db;
 
-// Connect to the database before starting the application server.
-var mongoClient = new mongodb.MongoClient(process.env.MONGODB_URI || "mongodb://localhost:27017/test",{ useUnifiedTopology: true });
-mongoClient.connect(function (err, client) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
+// // Connect to the database before starting the application server.
+// var mongoClient = new mongodb.MongoClient(process.env.MONGODB_URI || "mongodb://localhost:27017/test",{ useUnifiedTopology: true });
+// mongoClient.connect(function (err, client) {
+//   if (err) {
+//     console.log(err);
+//     process.exit(1);
+//   }
 
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
+//   // Save database object from the callback for reuse.
+//   db = client.db();
+//   console.log("Database connection ready");
 
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-});
+//   // Initialize the app.
+//   var server = app.listen(process.env.PORT || 8080, function () {
+//     var port = server.address().port;
+//     console.log("App now running on port", port);
+//   });
+// });
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
